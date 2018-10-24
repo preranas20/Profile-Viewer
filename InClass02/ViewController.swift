3320e17563d4f00a7a8fc5b675478e4aadaaec67
preranas20 <Prerana.Singh@ramanalearningcenter.com>
2018-10-24 09:46:38 -0400

    Initial Commit

---
+++ InClass02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InClass02
+//
+//  Created by Prerana Singh on 10/24/18.
+//  Copyright © 2018 Prerana Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
